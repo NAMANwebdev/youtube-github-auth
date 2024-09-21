76f071a6267336a0a6d49ef6658a8650c94baff1
Namandeep Singh <152621485+NAMANwebdev@users.noreply.github.com>
2024-09-22 00:25:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,41 +9,30 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Log environment variables
-console.log('Client ID:', process.env.GOOGLE_CLIENT_ID);
-console.log('Client Secret:', process.env.GOOGLE_CLIENT_SECRET);
-console.log('Callback URL:', process.env.CALLBACK_URL);
-
-// Home route
 app.get('/', (req, res) => {
   console.log("Home route accessed");
   res.render('index');
 });
 
-// Middleware for session handling
 app.use(session({ secret: 'your_secret_key', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set EJS as the view engine
 app.set('view engine', 'ejs');
 
-// Passport configuration
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.CALLBACK_URL,
   },
   function(accessToken, refreshToken, profile, done) {
-    profile.accessToken = accessToken; // Store access token in profile
+    profile.accessToken = accessToken;
     done(null, profile);
   }
 ));
 
-// Serialize and deserialize user
 passport.serializeUser((user, done) => {
   done(null, user);
 });
@@ -52,14 +41,12 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-// Route to start authentication
 app.get('/auth/google',
   passport.authenticate('google', {
     scope: ['profile', 'email', 'https://www.googleapis.com/auth/youtube.readonly']
   })
 );
 
-// Callback route
 app.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
@@ -67,7 +54,6 @@ app.get('/auth/google/callback',
   }
 );
 
-// Protected route
 app.get('/protected', (req, res) => {
   if (req.isAuthenticated()) {
     checkYouTubeSubscription(req.user.accessToken)
@@ -87,7 +73,6 @@ app.get('/protected', (req, res) => {
   }
 });
 
-// Function to check YouTube subscription
 async function checkYouTubeSubscription(accessToken) {
   const response = await axios.get('https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&mine=true', {
     headers: {
@@ -96,12 +81,11 @@ async function checkYouTubeSubscription(accessToken) {
   });
   
   const subscriptions = response.data.items;
-  const targetChannelId = process.env.TARGET_CHANNEL_ID; // Replace with your target channel ID
+  const targetChannelId = process.env.TARGET_CHANNEL_ID;
   return subscriptions.some(sub => sub.snippet.resourceId.channelId === targetChannelId);
 }
 
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
